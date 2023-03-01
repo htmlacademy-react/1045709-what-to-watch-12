849ac701eb92d5c4f67bbdf580e837b0e06fa88a
Samefral <galchun174@mail.ru>
2023-03-01 21:32:05 +0500

    added main-page react component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,8 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+import { FilmCardProps } from '../../types/film-card-props';
+
+function App(props: FilmCardProps): JSX.Element {
+  return < MainPage title={props.title} genre={props.genre} year={props.year}/>;
 }
 
 export default App;
