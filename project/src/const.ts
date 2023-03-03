d13adea83197d8715c65facea86e3f66788ce5fe
Samefral <galchun174@mail.ru>
2023-03-04 01:37:36 +0500

    added PrivateRoute component

--- project/src/const.ts
+++ project/src/const.ts
@@ -6,3 +6,9 @@ export enum AppRoute {
   AddReview = '/films/:id/review',
   VideoPlayer = '/player/:id'
 }
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
