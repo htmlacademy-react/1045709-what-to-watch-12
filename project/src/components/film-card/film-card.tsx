5ee12a01e1b19e842af342f11b1c14e6438bca00
Samefral <galchun174@mail.ru>
2023-03-07 17:33:26 +0500

    'film-card' component can receive data about film in props

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,11 +1,16 @@
-function FilmCard(): JSX.Element {
+type FilmCardProps = {
+  title: string;
+  posterSrc: string;
+};
+
+function FilmCard({title, posterSrc}: FilmCardProps): JSX.Element {
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
-        <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175" />
+        <img src={posterSrc} alt={title} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">Bohemian Rhapsody</a>
+        <a className="small-film-card__link" href="film-page.html">{title}</a>
       </h3>
     </article>
   );
