5c3b6d60bf0adbd4fd1cb25b205a4f2799e47d03
Samefral <galchun174@mail.ru>
2023-03-14 16:21:12 +0500

    film card img in <Link>

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -9,9 +9,11 @@ type FilmCardProps = {
 function FilmCard({film, onMouseEnterHandler}: FilmCardProps): JSX.Element {
   return (
     <article onMouseEnter={onMouseEnterHandler} className="small-film-card catalog__films-card">
-      <div className="small-film-card__image">
-        <img src={film.filmInfo.posterSrc} alt={film.filmInfo.title} width="280" height="175" />
-      </div>
+      <Link to={`films/${film.id}`}>
+        <div className="small-film-card__image">
+          <img src={film.filmInfo.posterSrc} alt={film.filmInfo.title} width="280" height="175" />
+        </div>
+      </Link>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`films/${film.id}`}>{film.filmInfo.title}</Link>
       </h3>
