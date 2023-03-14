194e0ed79763fd99da21097f953dc7473f478bac
Samefral <galchun174@mail.ru>
2023-03-15 02:54:34 +0500

    added 'film-card-player' component & card player playback on hover implemented

---
+++ project/src/components/film-card/film-card-player/film-card-player.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from 'react';
+
+const DELAY_BEFORE_PLAYBACK = 1000;
+
+type VideoPlayerProps = {
+  videoSrc: string;
+  posterSrc: string;
+};
+
+function FilmCardPlayer({videoSrc, posterSrc}: VideoPlayerProps): JSX.Element {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (videoRef.current) {
+        videoRef.current.volume = 0;
+        videoRef.current.play();
+      }
+    }, DELAY_BEFORE_PLAYBACK);
+  }, []);
+
+
+  return (
+    <video
+      poster={posterSrc}
+      ref={videoRef}
+      width="280"
+      height="175"
+    >
+      <source src={videoSrc} />
+    </video>
+  );
+}
+
+export default FilmCardPlayer;
