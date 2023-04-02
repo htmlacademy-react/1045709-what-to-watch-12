daefa8be7a54982ef47bd9392e70947e50cedbe6
Samefral <galchun174@mail.ru>
2023-04-02 21:59:07 +0500

    added & connected error-message component

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
+import ErrorMessage from './components/error-message/error-message';
 import { films } from './mocks/films';
 import { store } from './store';
 
@@ -12,6 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store = {store}>
+      <ErrorMessage />
       <App films = {films} headerFilm = {{title: 'The Grand Budapest Hotel', genre: 'Drama', year: 2014}} />
     </Provider>
   </React.StrictMode>
