f6c2e58fd3e890aee3c65f6583bb7dd93e926e2d
Samefral <galchun174@mail.ru>
2023-04-06 07:24:41 +0500

    sign in/out in the header depending on authorizationStatus

--- project/src/components/user-block/user-block.tsx
+++ project/src/components/user-block/user-block.tsx
@@ -1,17 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { logoutAction } from '../../store/api-actions';
+import { AuthorizationStatus, AppRoute } from '../../const';
 
 function UserBlock(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+  const onClick = () => {
+    dispatch(logoutAction());
+  };
+
   return (
-    <ul className="user-block">
-      <li className="user-block__item">
-        <div className="user-block__avatar">
-          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-        </div>
-      </li>
-      <li className="user-block__item">
-        <Link to="/login" className="user-block__link">Sign out</Link>
-      </li>
-    </ul>
+    authorizationStatus === AuthorizationStatus.Auth
+      ?
+      <ul className="user-block">
+        <li className="user-block__item">
+          <div className="user-block__avatar">
+            <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+          </div>
+        </li>
+        <li className="user-block__item">
+          <Link onClick={onClick} to={AppRoute.Login} className="user-block__link">Sign out</Link>
+        </li>
+      </ul>
+      :
+      <div className="user-block">
+        <Link to={AppRoute.Login} className="user-block__link">Sign in</Link>
+      </div>
+
   );
 }
 
